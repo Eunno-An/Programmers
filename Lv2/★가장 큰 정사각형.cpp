6c3903b6a9f33a14775267bcc42994cb21778f2e
Eunno An <12171647@inha.edu>
2021-07-26 22:51:58 +0900

    Update and rename (진행중)가장 큰 정사각형.cpp to ★가장 큰 정사각형.cpp

--- Lv2/★가장 큰 정사각형.cpp
+++ Lv2/★가장 큰 정사각형.cpp
@@ -1,4 +1,3 @@
-//효율성 문제 해결중
 #include <iostream>
 #include<vector>
 #include<queue>
@@ -11,17 +10,16 @@ int arr[1001][1001];
 int row, col;
 
 /*
-1    1  .   .   (y2, x2)
-1    1
-.
-.
-(y1, x1)        (y1, x2)
-(y1,x1)부터 (y1, x2)까지 1인지 확인
-(y2, x2)부터 (y1-1, x2)까지 1인지 확인
-solve(y1, x1, y2, x2, k) -> 현재 사각형의 왼쪽 하단, 오른쪽 상단의 좌표가 주어지고, 길이 k가 주어질 때, 사각형의 최대 넓이
-=이걸 좀 더 최적화 시키면?
-solve(int y1, int x1) = y1, x1을 오른쪽 아래 꼭짓점 으로 갖는 정사각형의 최대 넓이
-=
+DP의 느낌으로..풀었던 문제.
+              (y2, x2)
+
+(y1, x1)      (y1, x2)
+
+배열 탐색만 1000*1000 잡아먹고, 그럼 1초가 제한이라면, 더 쓸 수 있는건 100밖에 없음.
+잘 생각해보면, (y1,x2) 의 정사각형 최대 넓이는
+(y1, x2)와 왼쪽, 위, 왼쪽 대각선에 있는 세 수들의 min값이 됨. 왜냐? 붙어있으니까!
+그래서 O(3)번을 추가로 탐색하게 되고, 시간을 먹지 않는다.
+시간을 생각 안하고 풀어서 많이 헤맸던 문제.
 */
 int possible[1001][1001];
 bool ok(int y1, int x1, int y2, int x2) {
@@ -29,7 +27,11 @@ bool ok(int y1, int x1, int y2, int x2) {
 }
 int solve(int y1, int x1, int y2, int x2) {
 
-    if (ok(y1 + 1, x1, y2, x2 + 1) && arr[y1 + 1][x2 + 1] > 1) {
+    if(ok(y1-1, x1, y2, x2-1) && arr[y1-1][x2-1]){
+        
+    }
+    if (ok(y1 + 1, x1, y2, x2 + 1) && arr[y1 ][x2 ] > 1) {
+        
         return solve(y1 + 1, x1, y2, x2 + 1);
     }
     for (int i = x1; i <= x2; i++) {
@@ -44,11 +46,12 @@ int solve(int y1, int x1, int y2, int x2) {
             return (y2 - y1) * (y2 - y1);
         }
     }
+    arr[y1][x2]= (y1-y2+1)*(y1-y2+1);
     if (ok(y1 + 1, x1, y2, x2 + 1)) {
         return solve(y1 + 1, x1, y2, x2 + 1);
     }
     else {
-        return arr[y1][x2] = (y1-y2 + 1)*(y1-y2+1);
+        return arr[y1][x2];
     }
 
 }
@@ -65,12 +68,22 @@ int solution(vector<vector<int>> board)
         }
     }
     int max_square = -1;
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0; j < board[i].size(); j++) {
+    for (int i = 1; i < board.size(); i++) {
+        for (int j = 1; j < board[i].size(); j++) {
             if (arr[i][j] >= 1) {
-                int area = solve(i, j, i, j);
-                if (max_square < area)
-                    max_square = area;
+                int length = min(min(arr[i-1][j-1], arr[i][j-1]), arr[i-1][j]);
+                arr[i][j] = length+1;
+                if(max_square < arr[i][j]){
+                    max_square = arr[i][j];
+                }
+            }
+        }
+    }
+    
+    for(int i=0; i<board.size(); i++){
+        for(int j=0; j<board[i].size(); j++){
+            if(max_square < arr[i][j]){
+                max_square = arr[i][j];
             }
         }
     }
@@ -78,7 +91,7 @@ int solution(vector<vector<int>> board)
         answer = 0;
     }
     else {
-        answer = max_square;
+        answer = max_square * max_square;
     }
 
     return answer;
