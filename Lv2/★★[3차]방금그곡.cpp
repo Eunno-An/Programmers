94827086276ed5ebb70b3706591fb03a5b57b154
Eunno An <12171647@inha.edu>
2021-07-28 23:25:18 +0900

    Update and rename (진행중)[3차]방금그곡.cpp to ★★[3차]방금그곡.cpp
    이거 틀린거임.
    틀린 이유
    CDCDE, CDCDCDE를 비교하면, CDCDE가 있긴 함. 근데 나는 queue로 하나씩 보면서 비교하니까, 당연히 못찾음.
    내가 왜 find를 안썼는지? 문자열 길이가 너무 길어지면, 이거 탐색 시간만 엄청 오래 걸릴거라고 생각했음.그러나 힌트는 재생 시간에 있었음. 00:00을 초과해서 재생하는 노래가 없었고, 즉 문자열의 최대 길이는 00:00 ~ 23:59, 23 * 60 + 59 = 1439. 악보의 길이는 최대 1439임. 
    musicinfos에는 100개의 정보들이 있고, 100개의 정보들에 대해 O(m) * O(1439) * O(100)를 해도?. 대략 1초에 육박.. 1초 조금 넘긴 함. 근데 이건 진짜 최악일때 테스트케이스.
    왜냐 ..m이 1439의 반인 720정도일때 ,find에 필요한 비교횟수가 가장 많을듯. 
    이건 나중에 KMP 알고리즘으로도 연습해 보면 좋을듯. 다시 풀어보기.

---
+++ Lv2/★★[3차]방금그곡.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+//시간이 -일 때 주의하기
+//단순히 find로 풀어도 시간초과 안남.
+string solution(string m, vector<string> musicinfos) {
+    string answer = "";
+    
+    return answer;
+}
