01f2e4c3b59386f1cff9449d0d5caf57fa9f41ad
Eunno An <12171647@inha.edu>
2021-08-07 13:54:00 +0900

    Update and rename (진행중)후보키.cpp to ★후보키.cpp
    구현 좀 빡셈

--- Lv2/★후보키.cpp
+++ Lv2/★후보키.cpp
@@ -9,10 +9,10 @@ int solution(vector<vector<string>> relation) {
     int answer = 0;
 
     string table[21][9];
-    for (int i = 0; i < relation.size(); i++) 
-        for (int j = 0; j <relation[i].size(); j++)
+    for (int i = 0; i < relation.size(); i++)
+        for (int j = 0; j < relation[i].size(); j++)
             table[i][j] = relation[i][j];
-    
+
     //총 학생의 개수 = 튜플의 개수
     int total_Student = relation.size();
     int number_Column = relation[0].size();
@@ -22,21 +22,23 @@ int solution(vector<vector<string>> relation) {
         vector<bool> v(number_Column - i, false);
         v.insert(v.end(), i, true);
 
-        
+
         do {
             vector<int> column_key;
             for (int j = 0; j < v.size(); j++)
                 if (v[j]) column_key.push_back(j);
 
             bool isMinimal = true;
-            for (int j = 0; j < column_key.size(); j++) {
+            //column_key 중에서 최소성을 만족하는 key의 조합이 있는가를 찾는것.
+            //최소 1개 ~ column_key-1개를 선택한 것들의 조합을 따져보자
+            for (int j = 1; j < column_key.size(); j++) {
                 vector<bool> v1(column_key.size() - j, false);
-                v1.insert(v.end(), j, true);
+                v1.insert(v1.end(), j, true);//v1.insert(v.end(), j, true)라고 했다가 런타임 에러떴음.. 진짜 .. 이런 실수..
                 do {
                     vector<int> column_key1;
-                    for (int k = 0; k < v1.size(); k++) 
-                        if (v1[k]) column_key1.push_back(k);
-                    
+                    for (int k = 0; k < column_key.size(); k++)
+                        if (v1[k]) column_key1.push_back(column_key[k]);//이 부분도 column_key[k]를 넣어주어야 함.
+
                     if (!candidate_Keys.empty() && candidate_Keys.find(column_key1) != candidate_Keys.end()) {
                         isMinimal = false;
                         break;
@@ -47,32 +49,32 @@ int solution(vector<vector<string>> relation) {
             }
 
             if (!isMinimal)
-                break;
-                            
-            
-            
+                continue;
+
+
+
             map<vector<string>, int> result;
             //이제 table에서 키들의 정보로 뽑아서 맵에다 저장한다.
             for (int j = 0; j < relation.size(); j++) {//row 순회
                 vector<string> temp;
                 for (int k = 0; k < column_key.size(); k++) //col 순회
                     temp.push_back(relation[j][column_key[k]]);//relations의 j번째행, column_key[k] 열의 요소를 temp에다 넣는다. 키를 구하는 과정임.
-                
+
                 if (result.empty() || result.find(temp) == result.end())
                     result.insert(make_pair(temp, 1));
             }
             if (result.size() == total_Student)
                 candidate_Keys.insert(make_pair(column_key, true));
-            
+
         } while (next_permutation(v.begin(), v.end()));
     }
     answer = candidate_Keys.size();
 
     return answer;
 }
-int main(){
+int main() {
     vector<vector<string>>relations;
-    relations = {{"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}
-};
+    relations = { {"100","ryan","music","2"},{"200","apeach","math","2"},{"300","tube","computer","3"},{"400","con","computer","4"},{"500","muzi","music","3"},{"600","apeach","music","2"}
+    };
     solution(relations);
 }
