a6b3eb75c345ce478f0b7859d53279fac05f07e0
Eunno An <12171647@inha.edu>
2021-08-30 23:43:53 +0900

    Update (진행중)보석 쇼핑.cpp

--- Lv3/(진행중)보석 쇼핑.cpp
+++ Lv3/(진행중)보석 쇼핑.cpp
@@ -8,6 +8,11 @@ void printGems(vector<pair<string, int>>& gems) {
 	for (int i = 0; i < gems.size(); i++)
 		cout << gems[i].first << ' ' << gems[i].second << '\n';
 }
+bool compare(pair<int, int>a, pair<int, int>b) {
+	if (a.second - a.first == b.second - b.first)
+		return a.first < b.first;
+	return a.second - a.first < b.second - b.first;
+}
 vector<int> solution(vector<string> gems) {
 	vector<int> answer;
 	vector<string> original_gems = gems;
@@ -43,11 +48,16 @@ vector<int> solution(vector<string> gems) {
 		else
 			window[new_gems[i].first]+=new_gems[i].second;
 	}
+	vector<pair<int, int>> answers;//답의 후보가 여러개 일 수도 있다.
+	//그리고 반드시 중요한건, 우리는 중복을 줄인 상태에서 인덱스를 뽑았으니 
+	//그리디하게 풀린다는 보장이 없다.
+	//예를 들어, new_gems를 window로 순회했을 때, window size와 일치하는 구간이 총 두개 있다고 가정하자.
+	//근데, 실제 original_gems에서의 길이가 더 짧을수도 있다.
 	for (int i = num_gems; i < new_gems.size(); i++) {
 		if (window.size() == num_gems) {
 			end_idx = i - 1;
 			start_idx = end_idx - num_gems + 1;
-			break;
+			answers.push_back(make_pair(start_idx, end_idx));
 		}
 		string name_gem_now = new_gems[i].first;//new_gems에서 i번째 젬 이름
 		string name_gem_before = new_gems[i - num_gems].first;//new_gems에서 i-num_gems번째 젬 이름.
@@ -58,9 +68,9 @@ vector<int> solution(vector<string> gems) {
 			window.erase(name_gem_before);
 		//window에서 name_gem_now를 추가해 준다.
 		if (window.find(name_gem_now) == window.end())
-			window.insert(make_pair(name_gem_now, 1));
+			window.insert(make_pair(name_gem_now, new_gems[i].second));
 		else
-			window[name_gem_now]++;
+			window[name_gem_now]+=new_gems[i].second;
 
 
 	}
@@ -70,18 +80,26 @@ vector<int> solution(vector<string> gems) {
 		answer.push_back(original_gems.size() - new_gems.back().second + 1);
 	}
 	else {
-		int s = 0, e = 0;
-		for (int i = 0; i <= start_idx; i++)
-			s += new_gems[i].second;
-		for (int i = 0; i < end_idx; i++)
-			e += new_gems[i].second;
-		e += 1;
-		answer.push_back(s);
-		answer.push_back(e);
+		
+		for (int i = 0; i < answers.size(); i++) {
+			int s = 0, e = 0;
+			for (int j = 0; j <= answers[i].first; j++)
+				s += new_gems[j].second;
+			for (int k = 0; k < answers[i].second; k++)
+				e += new_gems[k].second;
+			e += 1;
+			answers[i].first = s;
+			answers[i].second = e;
+
+		}
+		
+		sort(answers.begin(), answers.end(), compare);
+		answer.push_back(answers.front().first);
+		answer.push_back(answers.front().second);
 	}
 
 	return answer;
 }
 int main() {
-	solution({ "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA" });
+	solution({ "AA", "AB", "AC", "AA", "AC" });
 }
