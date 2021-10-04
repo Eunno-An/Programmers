4df2ca700db49d61baaa6cea89104779667b02c1
Eunno An <12171647@inha.edu>
2021-10-04 23:42:40 +0900

    Update (진행중)순위.cpp

--- Lv3/(진행중)순위.cpp
+++ Lv3/(진행중)순위.cpp
@@ -12,51 +12,51 @@ int solution(int n, vector<vector<int>> results) {
     memset(matrix, 0, sizeof(matrix));
     memset(incoming, 0, sizeof(incoming));
     memset(outcoming, 0, sizeof(outcoming));
-    for(int i=0; i<results.size(); i++){
+    for (int i = 0; i < results.size(); i++) {
         int winner = results[i][0];
         int loser = results[i][1];
-        matrix[winner][loser]=1;
-        matrix[loser][winner]=-1;
+        matrix[winner][loser] = 1;
+        matrix[loser][winner] = -1;
         incoming[loser]++;
         outcoming[winner]++;
     }
     bool finish = true;
     int nodeNum = n;
-    while(finish){
-        finish=false;
-        
-        for(int i=1; i<=n; i++){
-            
-            if(incoming[i]+outcoming[i] == nodeNum-1){
+    while (finish) {
+        finish = false;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (incoming[i] + outcoming[i] == nodeNum - 1) {
                 nodeNum--;
                 finish = true;
                 answer++;
                 vector<int> losers;//i한테 진 애들
                 vector<int> winners;//i한테 이긴 애들
-                for(int j=1; j<=n && j != i; j++){
-                    if(matrix[i][j] == 1)//i가 j를 이겼을 때 
-                        losers.push_back(j);    
-                    else if(matrix[i][j] == -1)//i가 j한테 졌을 떄
+                for (int j = 1; j <= n; j++) {
+                    if (matrix[i][j] == 1)//i가 j를 이겼을 때 
+                        losers.push_back(j);
+                    else if (matrix[i][j] == -1)//i가 j한테 졌을 떄
                         winners.push_back(j);
                 }
-                for(int j=0; j<losers.size(); j++){
+                for (int j = 0; j < losers.size(); j++) {
                     int first = losers[j];
-                    for(int k=0; k<winners.size(); k++){
+                    for (int k = 0; k < winners.size(); k++) {
                         int second = winners[k];
-                        matrix[second][first]=1;
-                        matrix[first][second]=-1;
+                        matrix[second][first] = 1;
+                        matrix[first][second] = -1;
                     }
                 }
-                for(int j=1; j<=n; j++){
-                    matrix[i][j]=0;
-                    matrix[j][i]=0;
+                for (int j = 1; j <= n; j++) {
+                    matrix[i][j] = 0;
+                    matrix[j][i] = 0;
                 }
-                for(int j=1; j<=n; j++){
-                    int num_income =0, num_outcome=0;
-                    for(int k=1; k<=n; k++){
-                        if(matrix[j][k] == 1)
+                for (int j = 1; j <= n; j++) {
+                    int num_income = 0, num_outcome = 0;
+                    for (int k = 1; k <= n; k++) {
+                        if (matrix[j][k] == 1)
                             num_outcome++;
-                        else if(matrix[j][k] == -1)
+                        else if (matrix[j][k] == -1)
                             num_income++;
                     }
                     incoming[j] = num_income;
@@ -64,8 +64,11 @@ int solution(int n, vector<vector<int>> results) {
                 }
             }
         }
-        
+
     }
-    
+
     return answer;
+}
+int main() {
+    solution(5, { {4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5} });
 }
