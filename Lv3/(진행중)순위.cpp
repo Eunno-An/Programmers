f19f9be40f340860f722eee398e1b4f3695bc1f1
Eunno An <12171647@inha.edu>
2021-10-05 20:21:46 +0900

    Update (진행중)순위.cpp

--- Lv3/(진행중)순위.cpp
+++ Lv3/(진행중)순위.cpp
@@ -1,71 +1,54 @@
 #include <string>
 #include <vector>
 #include <cstring>
-#include <iostream>
 using namespace std;
 
+int dfs(vector<vector<int>> graph,vector<bool>& visit, int node, int n) {
+    if (visit[node] == true)
+        return 0;
+    int ret = 0;
+    visit[node] = true;
+    for (int i = 1; i <= n; i++) {
+        if (graph[node][i] == 1 && visit[i] == false) {//node랑 i랑 연결되어 있고 방문하지 않은 노드일 때
+            ret++;
+            ret += dfs(graph, visit, i, n);
+        }
+    }
+    
+    return ret;
+
+}
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
-    int matrix[101][101];
-    int incoming[101];
-    int outcoming[101];
-    memset(matrix, 0, sizeof(matrix));
-    memset(incoming, 0, sizeof(incoming));
-    memset(outcoming, 0, sizeof(outcoming));
+    
+    vector<vector<int>> graphA, graphB;
+    graphA.resize(101);
+    graphB.resize(101);
+    for (int i = 0; i < 101; i++) {
+        for (int j = 0; j < 101; j++) {
+            graphA[i].push_back(0);
+            graphB[i].push_back(0);
+        }
+        
+    }
+    vector<bool> visitA, visitB;
+    visitA.resize(101, false);
+    visitB.resize(101, false);
+    
     for (int i = 0; i < results.size(); i++) {
         int winner = results[i][0];
         int loser = results[i][1];
-        matrix[winner][loser] = 1;
-        matrix[loser][winner] = -1;
-        incoming[loser]++;
-        outcoming[winner]++;
+        graphA[winner][loser] = 1;
+        graphB[loser][winner] = 1;
     }
-    bool finish = true;
-    int nodeNum = n;
-    while (finish) {
-        finish = false;
-
-        for (int i = 1; i <= n; i++) {
-
-            if (incoming[i] + outcoming[i] == nodeNum - 1) {
-                nodeNum--;
-                finish = true;
-                answer++;
-                vector<int> losers;//i한테 진 애들
-                vector<int> winners;//i한테 이긴 애들
-                for (int j = 1; j <= n; j++) {
-                    if (matrix[i][j] == 1)//i가 j를 이겼을 때 
-                        losers.push_back(j);
-                    else if (matrix[i][j] == -1)//i가 j한테 졌을 떄
-                        winners.push_back(j);
-                }
-                for (int j = 0; j < losers.size(); j++) {
-                    int first = losers[j];
-                    for (int k = 0; k < winners.size(); k++) {
-                        int second = winners[k];
-                        matrix[second][first] = 1;
-                        matrix[first][second] = -1;
-                    }
-                }
-                for (int j = 1; j <= n; j++) {
-                    matrix[i][j] = 0;
-                    matrix[j][i] = 0;
-                }
-                for (int j = 1; j <= n; j++) {
-                    int num_income = 0, num_outcome = 0;
-                    for (int k = 1; k <= n; k++) {
-                        if (matrix[j][k] == 1)
-                            num_outcome++;
-                        else if (matrix[j][k] == -1)
-                            num_income++;
-                    }
-                    incoming[j] = num_income;
-                    outcoming[j] = num_outcome;
-                }
-            }
-        }
-
+    for (int i = 1; i <= n; i++) {
+        int a = dfs(graphA, visitA, i, n);
+        int b = dfs(graphB, visitB, i, n);
+        if (a + b == n - 1)
+            answer++;
     }
+        
+
 
     return answer;
 }
