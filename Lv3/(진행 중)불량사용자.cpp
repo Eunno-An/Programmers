928ee63557f66d84608e7076bef96ac00ff1429d
Eunno An <12171647@inha.edu>
2021-10-06 23:41:40 +0900

    Update (진행 중)불량사용자.cpp

--- Lv3/(진행 중)불량사용자.cpp
+++ Lv3/(진행 중)불량사용자.cpp
@@ -1,59 +1,66 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <iostream>
+#include <set>
 #include <algorithm>
+#include <iostream>
 using namespace std;
-
+set<set<string>> Bullys;
+void print_dict_bully_Info(map<string, set<string>> dict_bully){
+    for(auto it : dict_bully){
+        for(auto it2 : dict_bully[it.first])
+            cout << it2 << ' ';
+        cout << endl;
+    }
+}
+bool customCompare(string a, string b){//a가 userId, b가 bullyUserId
+    if(a.size() != b.size())
+        return false;
+    for(int i=0; i<a.size(); i++)
+        if(b[i] != '*' && a[i]!=b[i])
+            return false;
+    return true;
+}
+void get_number_of_cases_list(vector<string> banned_id, int idx, map<string, set<string>> dict_bully, set<string> tempBullys){
+    if(idx == banned_id.size()){
+        Bullys.insert(tempBullys);
+        return ;
+    }
+    for(int i=idx; i<banned_id.size(); i++){
+        string temp = banned_id[i];
+        //dict_bully[temp]에서 하나를 tempBullys에 넣고 잠시 지운다.
+        for(auto it : dict_bully[temp]){
+            tempBullys.insert(it);
+            dict_bully[temp].erase(dict_bully[temp].find(it));
+            get_number_of_cases_list(banned_id, idx+1, dict_bully, tempBullys);
+            dict_bully[temp].insert(temp);
+            tempBullys.erase(tempBullys.find(temp));
+        }
+    }
+    return ;
+}
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
-    
-    vector<vector<string>> users;
-    users.resize(8);
-    vector<vector<string>> list;
-    list.resize(banned_id.size());
-    for (int i = 0; i < user_id.size(); i++)
-        users[user_id[i].size()].push_back(user_id[i]);
-
-    for (int i = 0; i < banned_id.size(); i++) {
-        string banned = banned_id[i];
-        vector<int> pos_star;
-        for (int j = 0; j < banned.size(); j++)
-            if(banned[j] == '*')
-                pos_star.push_back(j);
-        
-        int banned_size = banned.size();
-        vector<string>& banned_list = users[banned_size];//user_id의 사이즈가 banned_id의 사이즈와 같은 것들
-        
-        for (int j = 0; j < banned_list.size(); j++) {
-            string& userID = banned_list[j];
-            string temp = userID;
-            for (int k = 0; k < pos_star.size(); k++) {
-                int star_pos = pos_star[k];
-                userID[star_pos] = '*';
-            }
-            if(userID == banned){
-                list[i].push_back(temp);
-            }
-            userID = temp;
-        }
-    }
-    map<string, int> m;
-    for(int i=0; i<list.size(); i++){
-        vector<string> temp;
-        for(int j=0; j<list[i].size(); j++){
-            if(m.find(list[i][j]) == m.end()){
-                m.insert(make_pair(list[i][j], 1));
-                temp.push_back(list[i][j]);
+    map<string, set<string>> dict_bully;
+    for(int i=0; i<banned_id.size(); i++){
+        string bullyUser=banned_id[i];
+        for(int j=0; j<user_id.size(); j++){
+            string user = user_id[j];
+            if(customCompare(user, bullyUser)){
+                if(dict_bully.find(bullyUser)==dict_bully.end()){
+                    set<string> s1;
+                    s1.insert(user);
+                    dict_bully.insert(make_pair(bullyUser, s1));
+                }
+                else
+                    dict_bully[bullyUser].insert(user);
             }
         }
-        list[i] = temp;
     }
-    for(int i=0; i<list.size(); i++){
-        for(int j=0; j<list[i].size(); j++)
-            cout << list[i][j] << ' ';
-        cout << '\n';
-    }
+    set<string> tempBullys;
+    get_number_of_cases_list(banned_id, 0, dict_bully, tempBullys);
     
-    return answer;
+    
+    
+    return answer=Bullys.size();
 }
