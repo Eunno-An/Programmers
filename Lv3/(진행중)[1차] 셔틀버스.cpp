6afd69804d4392b65a203aa91d51551b63fc3bc4
Eunno An <12171647@inha.edu>
2021-10-08 23:47:42 +0900

    Update (진행중)[1차] 셔틀버스.cpp

--- Lv3/(진행중)[1차] 셔틀버스.cpp
+++ Lv3/(진행중)[1차] 셔틀버스.cpp
@@ -31,28 +31,34 @@ void print_Crews_Info(vector<Crew*> Crews){
     for(int i=0; i<Crews.size(); i++)
         cout << Crews[i]->HH << ':' << Crews[i]->MM << '\n';
 }
-Crew* selectCrew(vector<Bus*> buses, vector<Crew*> crews, int n, int t, int m){
+Crew* getConArriveTime(vector<Bus*> buses, vector<Crew*> crews, int n, int t, int m){
     //버스들의 타임테이블이 주어질 때, crew들 중 이 안에 탈 수 있는 크루들만 뽑는다.
     //그 다음에 콘이 탈 수 있는 가장 늦은 시간을 뽑는다.
     int maximum_crew = n * m - 1;//반드시 콘이 타야함!
-    crew* Con;
+    Crew* Con = new Crew(0, 0);
+    vector<Crew*> waiting;
     Bus* lastBus = buses.back();
     for(int i=0; i<crews.size(); i++){
-        if(ret.size() == maximum_crew)
+        if(waiting.size() == maximum_crew)
             break;
         int HH = crews[i]->HH;
         int MM = crews[i]->MM;
         if(HH <=lastBus->HH && MM <= lastBus->MM)
-            ret.push_back(crews[i]);
+            waiting.push_back(crews[i]);
         else//이 이상은 이미 버스가 떠난것!
             break;
     }
-    if(maximum_crew > ret.size()){
+    if(maximum_crew > waiting.size()){
         Con->HH = lastBus->HH;
         Con->MM = lastBus->MM;
     }
     else{
-        
+        Con->HH = waiting.back()->HH;
+        Con->MM = waiting.back()->MM - 1;
+        if(Con->MM == -1){
+            Con->MM += 60;
+            Con->HH--;
+        }
     }
     return Con;
 }
@@ -79,6 +85,15 @@ string solution(int n, int t, int m, vector<string> timetable) {
     }    
     
     sort(Crews.begin(), Crews.end(), compare);
-      
+    Crew* Con = getConArriveTime(Buses, Crews, n, t, m);
+    
+    if(Con->HH < 10)
+        answer += "0";
+    answer += to_string(Con->HH);
+    answer += ":";
+    if(Con->MM < 10)
+        answer += "0";
+    answer += to_string(Con->MM);
+    
     return answer;
 }
