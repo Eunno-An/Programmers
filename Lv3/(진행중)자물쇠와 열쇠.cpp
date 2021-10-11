3362f49001efd689025e50680398429b1f55b7ca
Eunno An <12171647@inha.edu>
2021-10-11 19:50:07 +0900

    Update (진행중)자물쇠와 열쇠.cpp

--- Lv3/(진행중)자물쇠와 열쇠.cpp
+++ Lv3/(진행중)자물쇠와 열쇠.cpp
@@ -1,3 +1,6 @@
+//10.11 Lock이 다 1인경우에 대한 예외처리 성공
+//내 코드의 문제점: Lock이 4 by 4이고, 그 중간 2 by 2에 홈의 군집이 존재한다고 하자. 그때 key가 3 by 3이고, 어쩌다보니 중간에 겹쳐져 들어갔다고 하자.
+//그럼 key가 lock의 군집과 겹치는 부분이 서로 반대여야 함과 동시에, key가 lock의 홈 군집과 겹치지 않는 부분들 중 돌기 부분은 lock의 돌기와 만나면 안된다! 
 #include <string>
 #include <vector>
 #include <set>
@@ -5,7 +8,6 @@
 using namespace std;
 
 //Lock에서 H들을 포함할 수 있는 가장 적은 직사각형을 만들어라
-//10/11: 자물쇠 전체가 돌기일 경우 날 수 있는 segmentation 오류 발견 -> 34점에서 65점으로 정확도 향상.
 vector<vector<int>> getMinSquare(vector<vector<int>> lock) {
     vector<vector<int>> ret;
     int minY = 21, minX = 21;
@@ -54,15 +56,16 @@ void FlipKey(vector<vector<int>>& key) {
 bool checkSquare(vector<vector<int>> square, vector<vector<int>> key, int y, int x, int square_y, int square_x) {
     for (int i = 0; i < square_y; i++)
         for (int j = 0; j < square_x; j++) {
-            if (square[i][j] != key[i + y][j + x])
+            if (square[i][j] == key[i + y][j + x])
                 return false;
+            
         }
     return true;
 }
 bool is_all_one(vector<vector<int>> lock) {
     for (int i = 0; i < lock.size(); i++)
         for (int j = 0; j < lock[i].size(); j++)
-            if (lock[i][j])
+            if (lock[i][j] == 0)
                 return false;
     return true;
 }
@@ -83,11 +86,11 @@ bool is_key_available(set<vector<vector<int>>> squares, vector<vector<int>> key)
     return false;
 }
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
-    FlipKey(key);//편의를 위해 key의 모든 요소들을 flip시킨다.
+    
     bool answer = true;
     set<vector<vector<int>>> rotates;
     vector<vector<int>> square ;
-    if (is_all_one(lock)) //자물쇠 전체가 돌기인 경우
+    if (is_all_one(lock)) //자물쇠 전체가 돌기인경우(0)
         square = lock;
     else
         square = getMinSquare(lock);
@@ -99,6 +102,8 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
 
 }
 int main() {
-    cout << solution({ { 1, 1 }, { 1, 1 } }, { {0, 0}, {0, 0 } }) << endl;
+//    cout << solution({ { 1, 1 }, { 1, 1 } }, { {0, 0}, {0, 0 } }) << endl;
+    cout << solution( { {0, 0}, {0, 0 } }, { { 1, 1 }, { 1, 1 } }) << endl;
     cout << solution({ { 1, 1, 0, 1}, { 0, 1, 1, 0 }, { 1, 0, 1, 1 }, {0, 1, 1, 1 } }, { {1, 0, 1, 0, 1}, {0, 1, 0 , 0, 1}, {1, 0, 1, 1, 0}, {0, 0, 0, 1, 1}, {0, 1, 0 , 0, 1} }) << endl;
+    cout << solution({ {1, 1, 1}, {0, 0, 0 }, {1, 1, 0} }, { {1, 1, 1, 0}, {0, 0, 1, 0}, {1, 1, 1, 0}, {0, 1, 1, 1} }) << endl;
 }
