b8da22a79b98d77b1354ba34bc780f258b7f718e
Eunno An <12171647@inha.edu>
2021-10-15 23:48:18 +0900

    Update (진행중)10주차-2.cpp

--- weeklyChallenge/(진행중)10주차-2.cpp
+++ weeklyChallenge/(진행중)10주차-2.cpp
@@ -10,7 +10,13 @@ using namespace std;
 
 //a직선과 b직선의 관계를 파악한다.
 //앞에서부터 x, y의 계수, 그리고 상수
-
+int gcd(int a, int b) {
+    if (b == 0) return 1;
+    return gcd(b, a % b);
+}
+int lca(int a, int b) {
+    return a * b / gcd(a, b);
+}
 bool checkIntersection(vector<int> a, vector<int> b) {
     if (a[0] == 0 && b[0] == 0) //y = k 형태
         return false;
@@ -24,13 +30,62 @@ bool checkIntersection(vector<int> a, vector<int> b) {
     return true;
 }
 bool getIntersection_Point(vector<int> a, vector<int> b, pair<int, int>& ret) {
-     
+    int lcaX = lca(a[0], b[0]);
+    int lcaY = lca(a[1], b[1]);
+    //lcaX로 접점 찾기
+    //Y계수만 남기는 과정
+    int multiX1 = lcaX / a[0];
+    int multiX2 = lcaX / b[0];
+    for (int i = 1; i <= 2; i++) {
+        a[i] *= multiX1;
+        b[i] *= multiX2;
+    }
+    
+    int left = a[1] - b[1];//Y의 계수
+    int right = b[2] - a[2];//상수
+    if (left == 0) return false;
+    else {
+        if (right % left == 0) {
+            ret.second = right / left; // Y좌표.
+            int left1 = a[0];//X의 계수
+            if ((a[2] - a[1] * ret.second) * -1 % a[0] == 0) {
+                //X좌표가 정수라면
+                ret.first = ((a[2] - a[1]) * ret.second * -1 / a[0]);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
 }
-bool getIntersection_Point2(bool type, int a, vector<int> b, pair<int, int>& ret) {//type false : y=k 또는 type true: x=k와 어떤 직선과의 교점
-
+bool getIntersection_Point2(bool type, int a, vector<int> b, pair<int, int>& ret) {
+    //type false : y=k 또는 type true: x=k와 어떤 직선과의 교점
+    int left;//left: 상수
+    if (type == false) {
+        left = (b[1] * a - b[2]) * -1;
+        if (left % b[0] == 0) {//나머지가 0인 경우, 즉 가능한 경우
+            ret.first = left / b[0];
+            ret.second = a;
+            return true;
+        }
+        else
+            return false;
+    }
+    else {
+        left = (b[0] * a - b[2]) * -1;
+        if (left % b[1] == 0) {
+            ret.first = left / b[1];
+            ret.second = a;
+            return true;
+        }
+        else
+            return false;
+    }
 }
 bool getIntersection_Point3(int a, int b, pair<int, int>& ret) {//y=k와 x=l의 교점
-
+    ret.first = a;
+    ret.second = b;
+    return true;
 }
 
 vector<string> solution(vector<vector<int>> line) {
@@ -50,6 +105,8 @@ vector<string> solution(vector<vector<int>> line) {
                 if (line[j][0] == 0) //y축에 평행한 그래프임. 만날 일 없음
                     continue;
                 else if (line[j][1] == 0) {//x축에 평행한 그래프
+                    if ((line[j][2] * -1) % line[j][0] != 0)
+                        continue;
                     int x = (line[j][2] * -1) / line[j][0];
                     if (getIntersection_Point3(y, x, ret))
                         intersection_points.push_back(ret);
@@ -67,6 +124,8 @@ vector<string> solution(vector<vector<int>> line) {
                     continue;
                 else if (line[j][0] == 0) {//y축에 평행한 그래프
                     int y = (line[j][2] * -1) / line[j][1];
+                    if ((line[j][2] * -1) % line[j][1] != 0)
+                        continue;
                     if (getIntersection_Point3(y, x, ret))
                         intersection_points.push_back(ret);
                 }
