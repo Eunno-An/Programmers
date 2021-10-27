ad9981e0fa314bb35ba3d70db8af1270c51a8284
Eunno An <12171647@inha.edu>
2021-10-27 21:58:39 +0900

    Update (진행중)보행자 천국.cpp

--- Lv3/(진행중)보행자 천국.cpp
+++ Lv3/(진행중)보행자 천국.cpp
@@ -1,41 +1,49 @@
 #include <vector>
 #include <cstring>
-#include <iostream>
 using namespace std;
 
-const int MOD = 20170805;
-const int dy[2] = {1, 0};
-const int dx[2] = {0, 1};
+int MOD = 20170805;
+int cache[501][501][2];
+bool visit[501][501][2];
 
-int cache[501][501];//(y, x)에서 (m-1, n-1)까지 가는 경로의 수
-bool ok(int y, int x, int m, int n){
-    if(y >=0 && y < m && x >=0 && x < n) return true;
-    return false;
+const int dy[2] = { 1, 0 };
+const int dx[2] = { 0, 1 };
+vector<vector<int>> global_city_map;
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+bool ok(vector<vector<int>> city_map, int y, int x) {
+    return (y >= 0 && y < city_map.size() && x >= 0 && x < city_map[0].size()) ? true : false;
 }
-int function(vector<vector<int>> city_map, int y, int x, int dir){
-    if(city_map.size() -1 == y && city_map[0].size()-1 == x)
+int dfs(int y, int x, int dir) {
+    if (y == global_city_map.size() - 1 && x == global_city_map[0].size() - 1)
         return 1;
-    int& ret = cache[y][x];
-    if(ret != -1)
-        return ret;
+    int& ret = cache[y][x][dir];
+    if (ret != -1)
+        return ret % MOD;
     ret = 0;
-    for(int i=0; i<2; i++){
-        int next_y = y + dy[i];
-        int next_x = x + dx[i];
-        if(ok(next_y, next_x, city_map.size(), city_map[0].size()) && city_map[next_y][next_x] != 1){
-            if(city_map[next_y][next_x] == 2 && dir != i)//2인 경우 보행자 안전을 위해 좌회전이나 우회전이 금지.
-                continue;
+    visit[y][x][dir] = true;
+    for (int i = 0; i < 2; i++) {
+        int nextY = y + dy[i];
+        int nextX = x + dx[i];
+        if (ok(global_city_map, nextY, nextX) && global_city_map[nextY][nextX] != 1 && visit[nextY][nextX][i] == false) {
+            if (global_city_map[y][x] == 2) {
+                if (dir == i)
+                    ret += dfs(nextY, nextX, dir) % MOD;
+            }
             else
-                ret += function(city_map, next_y, next_x, i) % MOD;
+                ret += dfs(nextY, nextX, i) % MOD;
         }
     }
-    return ret;
+    visit[y][x][dir] = false;
+    return ret % MOD;
 }
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 int solution(int m, int n, vector<vector<int>> city_map) {
-    int answer = 0;
     memset(cache, -1, sizeof(cache));
-    answer += function(city_map, 0, 0, 0)% MOD;
-    
-    return cache[0][0];
+    memset(visit, false, sizeof(visit));
+    global_city_map = city_map;
+    int answer = 0;
+    answer += dfs(0, 0, 0);
+    return answer;
+}
+int main() {
+    solution(3, 6, { {0, 0, 0}, {0, 0, 0}, {0, 0, 0 } });
 }
