ffef7bea3fccd6202e320480a10bc601dc20ef59
Eunno An <12171647@inha.edu>
2021-10-28 19:43:57 +0900

    Update ★★★보행자 천국.cpp

--- Lv3/★★★보행자 천국.cpp
+++ Lv3/★★★보행자 천국.cpp
@@ -2,17 +2,17 @@
 //경주로 건설 문제와 매우 유사. 같은 문제라도 봐도 무방
 //BFS + DP
 //ISSUE 참고하기.
+
+//2021-10-28 코드 리팩토링
 #include <vector>
-#include <queue>
 #include <cstring>
+#include <queue>
 using namespace std;
 
 const int MOD = 20170805;
-const int dy[2] = {0, 1};
-const int dx[2] = {1, 0};
-int cache[501][501][2];
-bool visit[501][501][2];
-// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
+const int dy[2] = {1, 0};
+const int dx[2] = {0, 1};
+
 class Car{
     public:
     int y, x, dir;
@@ -22,53 +22,57 @@ class Car{
         this->dir = dir;
     }
 };
+//cache[y][x][dir] =(y, x)까지 이동할 수 있는 경우의 수.(이전에 dir방향으로 온 것임)
+int cache[501][501][2];
+bool visit[501][501][2];
+// 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 bool ok(int y, int x, int m, int n){
-    return (y >=0 && y < m && x >= 0 && x < n) ? true : false;
+    return (y >= m || x >= n) ? false : true;
 }
 int solution(int m, int n, vector<vector<int>> city_map) {
-    int answer = 0;
-    memset(cache, 0, sizeof(cache));   
+    memset(cache, 0, sizeof(cache));
     memset(visit, false, sizeof(visit));
+    int answer = 0;
     cache[0][0][0] = 1;
     cache[0][0][1] = 1;
     visit[0][0][0] = true;
     visit[0][0][1] = true;
     queue<Car*> move;
-    Car* car = new Car(0, 0, 0);
-    
-    move.push(car);
+    Car* start = new Car(0, 0, 0);
+    move.push(start);
     
     while(!move.empty()){
         Car* tempCar = move.front(); move.pop();
-        
-        for(int i=0; i<2; i++){
-            int nextY = tempCar->y + dy[i];
-            int nextX = tempCar->x + dx[i];
-            if(nextY >= m || nextX >= n)
-                continue;
-            if(city_map[nextY][nextX] == 1)
-                continue;
-            if(city_map[tempCar->y][tempCar->x] == 2){
-                int beforeDir = tempCar->dir;
-                if(beforeDir == i){
-                    if(!visit[nextY][nextX][i]){
-                        Car* newCar = new Car(nextY, nextX, i);
-                        move.push(newCar);
-                        visit[nextY][nextX][i] = true;
+        int y = tempCar->y;
+        int x = tempCar->x;
+        int before = tempCar->dir;
+           
+        for(int direction = 0; direction < 2; direction++){
+            int nextY = y + dy[direction];
+            int nextX = x + dx[direction];
+            if(ok(nextY, nextX, m, n)){
+                if(city_map[nextY][nextX] != 1){
+                    if(city_map[y][x] == 0){//제한 없이 갈 수 있는 경우
+                        if(visit[nextY][nextX][direction] == false){
+                            visit[nextY][nextX][direction] = true;
+                            Car* newCar = new Car(nextY, nextX, direction);
+                            move.push(newCar);
+                        }
+                        cache[nextY][nextX][direction] += cache[y][x][before] % MOD;
+                    }else{//city_map[nextY][nextX] == 2
+                        if(before == direction){
+                            if(visit[nextY][nextX][direction] == false){
+                                visit[nextY][nextX][direction] = true;
+                                Car* newCar = new Car(nextY, nextX, direction);
+                                move.push(newCar);
+                            }
+                            cache[nextY][nextX][direction] += cache[y][x][direction] % MOD;
+                        }
                     }
-                    cache[nextY][nextX][i] = (cache[nextY][nextX][i] % MOD + cache[tempCar->y][tempCar->x][i] % MOD) % MOD;
                 }
-            }
-            else{
-                    if(!visit[nextY][nextX][i]){
-                        Car* newCar = new Car(nextY, nextX, i);
-                         move.push(newCar);
-                        visit[nextY][nextX][i] = true;
-                    }
-                    cache[nextY][nextX][i] = (cache[nextY][nextX][i] % MOD + cache[tempCar->y][tempCar->x][tempCar->dir] % MOD) %MOD;
-                        
             }
         }
     }
-    return answer = (cache[m-1][n-1][0] % MOD + cache[m-1][n-1][1] % MOD) % MOD;
+    
+    return answer = (cache[m-1][n-1][0]%MOD + cache[m-1][n-1][1]%MOD) % MOD;
 }
