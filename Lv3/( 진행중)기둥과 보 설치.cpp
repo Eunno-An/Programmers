6e1238ed0b5de8b80045ce77d42807759008303b
Eunno An <12171647@inha.edu>
2021-11-05 23:24:19 +0900

    Update ( 진행중)기둥과 보 설치.cpp

--- Lv3/( 진행중)기둥과 보 설치.cpp
+++ Lv3/( 진행중)기둥과 보 설치.cpp
@@ -1,73 +1,136 @@
-//이런식으로 하면 문제 발생. 
-//기둥과 보가 맞닿아 있는 곳을 삭제할 경우, 보의 왼쪽 부분에 있었던 곳인지, 아니면 오른쪽 부분에 있었던 곳인지 헷갈림.
-//기둥과 보가 맞닿는 케이스에 대해 좀 더 생각해 볼 필요가 있음.
-
 #include <string>
 #include <vector>
 #include <cstring>
+#include <queue>
 using namespace std;
+class Point{//상하좌우로 뭐가 있는지.
+    public: 
+    int u, d, l, r;
+    Point(int u, int d, int l, int r){
+        this->u= u;
+        this->d =d;
+        this->l = l;
+        this->r = r;
+    }
+};
+Point* matrix[101][101];
+bool visit[101][101];
+//0 : 기둥, 1 : 보
+bool is_able_set(int y, int x, int a){
+    if(a == 0){//기둥 설치
+        //바닥 위에 있거나(y == 0)
+        //보의 한쪽 끝 부분 위에 있거나(matrix[y][x]->l == 1 || matrix[y][x]->r == 1)
+        //다른 기둥 위에 있을 때(matrix[y][x]->d == 1)
+        if(y == 0 || matrix[y][x]->l == 1 || matrix[y][x]->r == 1 || matrix[y][x]->d == 1) {
+            matrix[y][x]->u = 1;
+            matrix[y+1][x]->d = 1;
+            return true;
+        }else
+            return false;
+    }
+    else{//보 설치
+        //한쪽 끝 부분이 기둥 위에 있거나(matrix[y][x]->d == 1)
+        //양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 한다(matrix[y][x]->l == 1 && matrix[y][x+1]->l == 1)
+        if(matrix[y][x]->d == 1 || (matrix[y][x]->l == 1 && matrix[y][x+1]->l == 1)){
+            matrix[y][x]->r = 1;
+            matrix[y][x+1]->l = 1;
+            return true;
+        }else
+            return false;
+    }
+        
+}
+bool is_able_delete(int y, int x, int a){
+    if(a == 0){//기둥 삭제
+        
+        //matrix[y+1][x]지점에 무언가 받쳐줄게 있어야 한다.
+        //(y+1, x)지점의 양쪽이 보로 연결되어 있을때(matrix[y+1][x]->l == 1 && matrix[y+1][x]->r == 1) 
+        
+        if(matrix[y+1][x]->l == 1 && matrix[y+1][x]->r == 1){
+            matrix[y][x]->u = 0;
+            matrix[y+1][x]->d = 0;
+            return true;
+        }else
+            return false;
+    }else{//보 삭제
+        //보 삭제는 
+        //(y, x+1)지점이 기둥으로 받쳐져 있고 
+        //(y, x)지점이 기둥으로 받쳐져 있을 때
+        if(matrix[y][x+1]->d == 1 && matrix[y][x]->d == 1){
+            matrix[y][x]->r = 0;
+            matrix[y][x+1]->l = 0;
+            return true;
+        }else
+            return false;
+    }
+}
+void set_structure(int y, int x, int a){
+    if(is_able_set(y, x, a))
+        return;
+}
+void delete_structure(int y, int x, int a){
+    if(is_able_delete(y, x, a))
+        return;
+}
 
-//보는 항상 왼쪽의 좌표만, 기둥은 항상 아래 좌표만 기입하면 됨.
-//우리는 좌표를 위 아래 뒤집어서 생각할 것이고, 그렇게 되면 위-아래의 조건도 바뀌게 됨.
-//그리고 문제에서 원하는 (x, y) 좌표도 (y, x)로 생각해야 함. ex) 문제에서 (2, 1)은 내 좌표에서는 (1, 2)가 됨. -> 반드시 나중에 좌표 뒤집어서 리턴하기!
-
-//matrix[y][x] == -1? 아무것도 없음. 
-//0 == 기둥 아래(기둥을 설치하는 곳)
-//1 == 기둥 위
-//2 == 보 왼쪽(보를 설치하는 곳)
-//3 == 보 오른쪽
-//4 == 기둥과 기둥이 맞닿음.
-//5 == 기둥과 보가 맞닿음.
-//6 == 보와 보가 맞닿음.
-int matrix[101][101];
 
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
     vector<vector<int>> answer;
-    memset(matrix, -1, sizeof(matrix));
+    
+    for(int i=0; i<101; i++)
+        for(int j=0; j<101; j++)
+            matrix[i][j] = new Point(0, 0, 0, 0);
     
     for(int i=0; i<build_frame.size(); i++){
-        //우리 좌표에서는 반대로 생각하기! (x, y) -> (y, x)
         int y = build_frame[i][0];
         int x = build_frame[i][1];
-        //a =  설치 또는 삭제할 구조물의 종류. 0 = 기둥, 1 = 보
         int a = build_frame[i][2];
-        //b = 구조물을 설치할 지, 혹은 삭제할 지를 나타냄. 0 = 삭제, 1 = 설치
         int b = build_frame[i][3];
+        if(b == 1)
+            set_structure(y, x, a);
+        else
+            delete_structure(y, x, a);
+    }
+    int startX;
+    for(int i=0; i<=n; i++)
+        if(matrix[0][i]->u == 1){
+            startX = i;
+            break;
+        }
+    
+    queue<vector<int>> temp;
+    vector<int> firstStructure = {0, startX, 0};
+    temp.push(firstStructure);
+    
+    
+    while(!temp.empty()){
+        vector<int> structure = temp.front(); temp.pop();
+        answer.push_back(structure);
+        int y = structure[0];
+        int x = structure[1];
+        visit[y][x]=true;
         
-        //벽면을 벗어나게 기둥, 보를 설치하는 경우는 없다!
-        //바닥에 보를 설치하는 경우는 없다.
-        //구조물이 겹치도록 설치하는 경우와, 없는 구조물을 삭제하는 경우는 입력으로 주어지지 않습니다.
-        if(b == 0){//구조물을 설치
-            if(a == 0){//기둥을 설치할 경우!
-                if(y == 0){//바닥 위에 있는 경우
-                    matrix[y][x] = 0;
-                    matrix[y+1][x] = 1;
-                }else if(matrix[y][x] == 2 || matrix[y][x] == 3){//보의 한쪽 끝에 설치 할 경우
-                    matrix[y][x] = 5;
-                    matrix[y+1][x] = 1;
-                }else if(matrix[y][x] == 1){//다른 기둥 위에 있는 경우
-                    matrix[y][x] = 4;
-                    matrix[y+1][x] = 1;
-                }
-            }
-            else{//보를 설치할 경우!
-                if(matrix[y][x] == 1){//한쪽 끝 부분이 기둥 위에.
-                    matrix[y][x] = 5;
-                    matrix[y][x+1] = 3;
-                }else if(matrix[y][x+1] == 2 && matrix[y][x] == 3){//양쪽 끝 부분이 다른 보와 동시에 연결되어 있는 경우
-                    matrix[y][x] = 6;
-                    matrix[y][x+1] = 6;
-                }
-            }
-        }else{//구조물을 삭제
-            if(a == 0){//기둥을 삭제할 경우!
-                
-            }
-            else{//보를 삭제할 경우!
-                
-            }
+        int up = matrix[y][x]->u;
+        int down = matrix[y][x]->d;
+        int left = matrix[y][x]->l;
+        int right = matrix[y][x]->r;
+        vector<int> nextStructure;
+        if(up == 1 && visit[y+1][x] == false){
+            nextStructure = {y+1, x, 0};
+            temp.push(nextStructure);
+        }
+        if(down == 1 && visit[y-1][x] == false){
+            nextStructure = {y-1, x, 0};
+            temp.push(nextStructure);
+        }
+        if(left == 1 && visit[y][x-1] == false){
+            nextStructure = {y, x-1, 1};
+            temp.push(nextStructure);
+        }
+        if(right == 1 && visit[y][x+1] == false){
+            nextStructure = {y, x+1, 1};
+            temp.push(nextStructure);
         }
     }
-    
     return answer;
 }
