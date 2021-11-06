0e84becc93a2cde6660a3c49f17d9b81341c0836
Eunno An <12171647@inha.edu>
2021-11-06 23:00:45 +0900

    Update ( 진행중)기둥과 보 설치.cpp

--- Lv3/( 진행중)기둥과 보 설치.cpp
+++ Lv3/( 진행중)기둥과 보 설치.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <cstring>
 #include <queue>
+#include <algorithm>
 using namespace std;
 class Point {//상하좌우로 뭐가 있는지.
 public:
@@ -14,9 +15,9 @@ public:
     }
 };
 Point* matrix[101][101];
-bool visit[101][101][2];
+bool visit[101][101];
 //0 : 기둥, 1 : 보
-bool is_able_set(int y, int x, int a) {
+bool is_able_set(int x, int y, int a) {
     if (a == 0) {//기둥 설치
         //바닥 위에 있거나(y == 0)
         //보의 한쪽 끝 부분 위에 있거나(matrix[y][x]->l == 1 || matrix[y][x]->r == 1)
@@ -32,7 +33,7 @@ bool is_able_set(int y, int x, int a) {
     else {//보 설치
         //한쪽 끝 부분이 기둥 위에 있거나(matrix[y][x]->d == 1)
         //양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 한다(matrix[y][x]->l == 1 && matrix[y][x+1]->l == 1)
-        if (matrix[y][x]->d == 1 || (matrix[y][x]->l == 1 && matrix[y][x + 1]->l == 1)) {
+        if (matrix[y][x]->d == 1 || matrix[y][x + 1]->d == 1 || (matrix[y][x]->l == 1 && matrix[y][x + 1]->r == 1)) {
             matrix[y][x]->r = 1;
             matrix[y][x + 1]->l = 1;
             return true;
@@ -42,7 +43,7 @@ bool is_able_set(int y, int x, int a) {
     }
 
 }
-bool is_able_delete(int y, int x, int a) {
+bool is_able_delete(int x, int y, int a) {
     if (a == 0) {//기둥 삭제
 
         //matrix[y+1][x]지점에 무언가 받쳐줄게 있어야 한다.
@@ -69,32 +70,39 @@ bool is_able_delete(int y, int x, int a) {
             return false;
     }
 }
-void set_structure(int y, int x, int a) {
-    if (is_able_set(y, x, a))
+void set_structure(int x, int y, int a) {
+    if (is_able_set(x, y, a))
         return;
 }
-void delete_structure(int y, int x, int a) {
-    if (is_able_delete(y, x, a))
+void delete_structure(int x, int y, int a) {
+    if (is_able_delete(x, y, a))
         return;
 }
 
-
+bool compare(vector<int> a, vector<int> b) {
+    if (a[0] == b[0]) {
+        if (a[1] == b[1])
+            return a[2] < b[2];
+        return a[1] < b[1];
+    }
+    return a[0] < b[0];
+}
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
     vector<vector<int>> answer;
 
     for (int i = 0; i < 101; i++)
         for (int j = 0; j < 101; j++)
             matrix[i][j] = new Point(0, 0, 0, 0);
-    memset(visit, false, sizeof(visit));
+
     for (int i = 0; i < build_frame.size(); i++) {
         int x = build_frame[i][0];
         int y = build_frame[i][1];
-        int a = build_frame[i][2];
-        int b = build_frame[i][3];
+        int a = build_frame[i][2];//1: 보, 0: 기둥
+        int b = build_frame[i][3];//1:설치, 0: 삭제
         if (b == 1)
-            set_structure(y, x, a);
+            set_structure(x, y, a);
         else
-            delete_structure(y, x, a);
+            delete_structure(x, y, a);
     }
     int startX;
     for (int i = 0; i <= n; i++)
@@ -104,49 +112,54 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
         }
 
     queue<vector<int>> temp;
-    vector<int> firstStructure = { 0, startX, 0 };
-    temp.push(firstStructure);
 
-
+    vector<int> pos = { 0, startX };
+    temp.push(pos);
     while (!temp.empty()) {
-        vector<int> structure = temp.front(); temp.pop();
-        
-        int y = structure[0];
-        int x = structure[1];
-        
-        int up = matrix[y][x]->u;
-        int down = matrix[y][x]->d;
-        int left = matrix[y][x]->l;
-        int right = matrix[y][x]->r;
-        vector<int> nextStructure;
-        int nextSt;
-        if (up == 1 && visit[y + 1][x][0] == false) {
-            nextStructure = { y + 1, x, 0 };
-            visit[y][x][0] = true;
-            answer.push_back(structure);
-            temp.push(nextStructure);
+        vector<int> tempPos = temp.front(); temp.pop();
+        int here_y = tempPos[0];
+        int here_x = tempPos[1];
+
+        visit[here_y][here_x] = true;
+
+        int up = matrix[here_y][here_x]->u;
+        int down = matrix[here_y][here_x]->d;
+        int left = matrix[here_y][here_x]->l;
+        int right = matrix[here_y][here_x]->r;
+
+        vector<int> nextPos;
+        vector<int> nowStructure;
+        if (up == 1 && visit[here_y + 1][here_x] == false) {
+            nextPos = { here_y + 1, here_x };
+            temp.push(nextPos);//다음 좌표를 넣는다.
+            nowStructure = { here_y, here_x, 0 };
+            answer.push_back(nowStructure);//현재 구조물을 넣는다.
         }
-        if (down == 1 && visit[y - 1][x][0] == false) {
-            nextStructure = { y - 1, x, 0 };
-            visit[y][x][0] = true;
-            answer.push_back(structure);
-            temp.push(nextStructure);
+        if (down == 1 && visit[here_y - 1][here_x] == false) {
+            nextPos = { here_y - 1, here_x };
+            temp.push(nextPos);
+            nowStructure = { here_y - 1, here_x, 0 };
+            answer.push_back(nowStructure);
         }
-        if (left == 1 && visit[y][x - 1][1] == false) {
-            nextStructure = { y, x - 1, 1 };
-            visit[y][x][0] = true;
-            answer.push_back(structure);
-            temp.push(nextStructure);
+        if (left == 1 && visit[here_y][here_x - 1] == false) {
+            nextPos = { here_y, here_x - 1 };
+            temp.push(nextPos);
+            nowStructure = { here_y, here_x - 1, 1 };
+            answer.push_back(nowStructure);
         }
-        if (right == 1 && visit[y][x + 1][1] == false) {
-            nextStructure = { y, x + 1, 1 };
-            visit[y][x][0] = true;
-            answer.push_back(structure);
-            temp.push(nextStructure);
+        if (right == 1 && visit[here_y][here_x + 1] == false) {
+            nextPos = { here_y, here_x + 1 };
+            temp.push(nextPos);
+            nowStructure = { here_y, here_x, 1 };
+            answer.push_back(nowStructure);
         }
     }
+    for (int i = 0; i < answer.size(); i++)
+        swap(answer[i][0], answer[i][1]);
+
+    sort(answer.begin(), answer.end(), compare);
     return answer;
 }
 int main() {
-    solution(5, {{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}} );
+    solution(5, { {1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1} });
 }
