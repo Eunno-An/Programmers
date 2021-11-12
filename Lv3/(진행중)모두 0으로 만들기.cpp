5b99fad1a79fac35020bbdf262a7b6feb4ba8d04
Eunno An <12171647@inha.edu>
2021-11-12 23:03:46 +0900

    Update (진행중)모두 0으로 만들기.cpp

--- Lv3/(진행중)모두 0으로 만들기.cpp
+++ Lv3/(진행중)모두 0으로 만들기.cpp
@@ -1,19 +1,91 @@
 #include <string>
 #include <vector>
-
+#include <queue>
+#include <cmath>
+#include <cstring>
 using namespace std;
+bool visit[300001];
+long long weights[300001];
+//모든 점들의 가중치를 0으로 만드는 최소 행동의 개수
+class Node {
+public:
+    int nodeNum;
+    long long weight;
+    Node(int n, long long w) {
+        nodeNum = n;
+        weight = w;
+    }
+};
+class Edge {
+public:
+    Node* node1;
+    Node* node2;
+    Edge(Node* n1, Node* n2) {
+        node1 = n1;
+        node2 = n2;
+    }
+};
+int function(Node* node1, Node* node2) {
+    int num_node1 = node1->nodeNum;
+    int num_node2 = node2->nodeNum;
+    long long w_node1 = node1->weight;
+    long long w_node2 = node2->weight;
 
-//단순하게 정점들의 합을 봐서는 모든 트리의 가중치를 0으로 만들 수 있는지 여부를 알 수가 없다.
-//왜냐하면 정점들이 연결된 간선에 대해서 수행해야 되는 것이기 때문에..
+    if (w_node1 == 0)
+        return 0;
 
-// 모든 점들의 가중치를 0으로 만들어야 하는 '최소' 연산의 개수.
-
-// 리프 노드들을 찾아서 배열에 넣는다. 그리고 깊이 순대로 정렬한다. -> 근데 이 문제에서는 리프가 따로 안나옴.
-// -> **트리의 특성을 생각해보면, 트리를 펼쳐보면 그래프 모양이 되고, 어떤 점에서 시작해서 이어진 간선을 따라서 퍼져나가는 형식으로 함수를 짜면 된다.
-// -> 즉, 어떤 점을 아무거나 잡아서 그리디하게 풀면 똑같을 것 같다.
-// (X)모든 리프노드들에 대해서, 주어진 연산을 루트까지 따라가서 수행한다. 
-
+    weights[num_node1] = 0;
+    weights[num_node2] += w_node1;
+    node1->weight = 0;
+    node2->weight = weights[num_node2];
+    return w_node1;
+}
 long long solution(vector<int> a, vector<vector<int>> edges) {
-    long long answer = -2;
+    long long answer = 0;
+
+    vector<vector<Node*>> adj_list(a.size(), vector<Node*>());
+
+    memset(visit, false, sizeof(visit));
+    memset(weights, 0, sizeof(weights));
+    for (int i = 0; i < a.size(); i++)
+        weights[i] = a[i];
+    for (int i = 0; i < edges.size(); i++) {
+        Node* node1 = new Node(edges[i][0], a[edges[i][0]]);
+        Node* node2 = new Node(edges[i][1], a[edges[i][1]]);
+        adj_list[node1->nodeNum].push_back(node2);
+        adj_list[node2->nodeNum].push_back(node1);
+    }
+    queue<Node*> waits;
+    for (int i = 0; i < adj_list.size(); i++)
+        if (adj_list[i].size() == 1) {//양 방향 그래프이므로, 리프노드의 인접한 노드 개수는 1일 수밖에 없음.
+            Node* leafNode = new Node(i, a[i]);
+            waits.push(leafNode);
+
+        }
+
+    while (!waits.empty()) {
+        Node* here = waits.front(); waits.pop();
+
+        int here_num = here->nodeNum;
+        long long here_weight = here->weight;
+        visit[here_num] = true;
+        for (int i = 0; i < adj_list[here_num].size(); i++) {
+            Node* next = adj_list[here_num][i];
+            if (visit[next->nodeNum] == true)
+                continue;
+
+            answer += abs(function(here, next));
+
+            waits.push(next);
+        }
+    }
+
+    for (int i = 0; i < a.size(); i++)
+        if (weights[i] != 0)
+            return -1;
+
     return answer;
+}
+int main() {
+    solution({ -5,0,2,1,2 }, { {0, 1}, {3, 4}, {2, 3}, {0, 3} });
 }
