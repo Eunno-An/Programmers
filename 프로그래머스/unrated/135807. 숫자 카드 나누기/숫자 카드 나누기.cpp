276f3f0c5197119680dd0e18e534a6f5ed628356
EN <12171647@inha.edu>
2022-12-15 18:34:49 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.cpp
@@ -1,50 +1,31 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-vector<int> candidate, my, other;
-
-void find_candidate(int num){
-    candidate.push_back(num);
-    for(int i = num / 2; i > 1; i--) //my의 최소 값인 num의 약수들 찾기
-        if(num % i == 0) candidate.push_back(i);
-    
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a%b);
 }
-
-int check(){
-    int ret = 0;
-    for(auto c : candidate){
-        int flag = 1;
-        for(int i = 0; i < my.size(); i++){
-            if(my[i] % c != 0 || other[i] % c == 0){ //조건 불만족
-                flag = 0;
-                break; //이미 조건을 만족하지 못하므로 나머지 검사 x
-            }
-        }
-        //후보군은 내림차순으로 정렬돼 있으므로 찾았으면 바로 종료
-        if(flag){  
-            ret = c;
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    int a = A[0], b = B[0];
+    for(int i=1; i<A.size(); i++){
+        a = gcd(A[i], a);
+        b = gcd(B[i], b);
+    }
+    answer = a;
+    for(int i=0; i<B.size(); i++){
+        if(B[i] % a == 0){
+            answer = 0;
             break;
         }
     }
-    return ret; //ret 초기값이 0이므로 조건을 만족하는 수를 찾지 못하면 0을 반환함.
-}
-
-int solution(vector<int> arrayA, vector<int> arrayB) {
-    int answer = 0;
-    sort(arrayA.begin(), arrayA.end());
-    sort(arrayB.begin(), arrayB.end());
-    
-    find_candidate(arrayA[0]);
-    my = arrayA, other = arrayB;
-    answer = max(answer, check());
-    
-    candidate.clear();
-    my = arrayB, other = arrayA;
-    find_candidate(arrayB[0]);
-    answer = max(answer, check());
-    
-    return answer; //answer 초기값이 0이므로 조건을 만족하는 a를 찾지 못하면 0을 반환함.
+    for(int i=0; i<A.size(); i++){
+        if(A[i] % b == 0){
+            if(answer == 0)
+                return 0;
+        }
+    }
+    return max(a, b);
 }
